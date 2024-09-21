cfaf72d09450fa8cb286b086ba080505906accf7
Miron <mironnesvetaylov@gmail.com>
2024-09-21 15:18:19 +0300

    Change neovim terminal providers internals as well

--- lua/rittli/tasks/terminal_providers/neovim.lua
+++ lua/rittli/tasks/terminal_providers/neovim.lua
@@ -15,39 +15,36 @@ local function CreateNeovimTerminalHandler(info)
   return terminal_handler
 end
 
----@param create_window_for_terminal fun(bufnr: number): nil
----@return NeovimTerminalProvider
-local function CreateNeovimTerminalProvider(create_window_for_terminal)
-  ---@class NeovimTerminalProvider: ITerminalProvider
-  local terminal_provider = {}
-
-  terminal_provider.create = function(data)
-    local bufnr = vim.api.nvim_create_buf(true, false)
-    create_window_for_terminal(bufnr)
-    local job_id = vim.fn.termopen(vim.o.shell, { detach = true, env = data.env })
-    return CreateNeovimTerminalHandler({ job_id = job_id })
-  end
-
-  return terminal_provider
-end
 
 function M.CreateTabProvider()
-  local function create_window_for_terminal(bufnr)
+  ---@class NeovimTabTerminalProvider: ITerminalProvider
+  local provider = {}
+
+  provider.create = function(data)
+    local bufnr = vim.api.nvim_create_buf(true, false)
     vim.cmd("tabnew")
     local tab_bufnr = vim.fn.bufnr("%")
     vim.api.nvim_command("b " .. bufnr)
     vim.api.nvim_buf_delete(tab_bufnr, {})
+    local job_id = vim.fn.termopen(vim.o.shell, { detach = true, env = data.env })
+    return CreateNeovimTerminalHandler({ job_id = job_id })
   end
 
-  return CreateNeovimTerminalProvider(create_window_for_terminal)
+  return provider
 end
 
 function M.CreateSplitProvider()
-  local function create_window_for_terminal(bufnr)
-    vim.api.nvim_open_win(bufnr, true, {split = "below", height = 15})
+  ---@class NeovimSplitTerminalProvider: ITerminalProvider
+  local provider = {}
+
+  provider.create = function(data)
+    local bufnr = vim.api.nvim_create_buf(true, false)
+    vim.api.nvim_open_win(bufnr, true, { split = "below", height = 15 })
+    local job_id = vim.fn.termopen(vim.o.shell, { detach = true, env = data.env })
+    return CreateNeovimTerminalHandler({ job_id = job_id })
   end
 
-  return CreateNeovimTerminalProvider(create_window_for_terminal)
+  return provider
 end
 
 return M
